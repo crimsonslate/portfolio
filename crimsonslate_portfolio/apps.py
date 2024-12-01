2ae2299d9d78db9a2014648b9e5787d917861560
Blake Nall (darthnall) <dev@darthnall.com>
2024-12-01 13:40:18 -0600

    Simplified forms and removed label

--- crimsonslate_portfolio/apps.py
+++ crimsonslate_portfolio/apps.py
@@ -4,4 +4,4 @@ from django.apps import AppConfig
 class CrimsonslatePortfolioConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "crimsonslate_portfolio"
-    label = "portfolio"
+    verbose_name = "Crimsonslate Portfolio Django Application"
