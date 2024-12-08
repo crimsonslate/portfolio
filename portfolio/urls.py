1c0dfb63bf1b6157764bac116ad422e9a8ffb159
darthnall <dev@darthnall.com>
2024-12-08 17:32:50 -0600

    Added docs and migrations

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -6,6 +6,7 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("admin/docs/", include("django.contrib.admindocs.urls")),
+    path("docs/", include("docs.urls")),
     path("", include("crimsonslate_portfolio.urls")),
 ]
 
